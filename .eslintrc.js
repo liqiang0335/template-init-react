420b10ce0271c7433d46253485b27b1d403615f1
liqiang <liqiang0335@qq.com>
2020-04-07 08:56:41 +0800

    add

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,26 +4,27 @@ module.exports = {
     browser: true,
     commonjs: true,
     node: true,
-    es6: true
+    es6: true,
   },
   globals: {
-    importName: true
+    $: true,
+    path: true,
   },
   plugins: ["react-hooks"],
   parser: "babel-eslint",
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: "module",
-    ecmaFeatures: { jsx: true }
+    ecmaFeatures: { jsx: true },
   },
   rules: {
     semi: 2,
     "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
+    "react-hooks/exhaustive-deps": "off",
     "react/react-in-jsx-scope": "off",
     "react/prop-types": "off",
     "react/display-name": "off",
     "no-irregular-whitespace": "off",
-    "no-console": "warn"
-  }
+    "no-console": "warn",
+  },
 };
